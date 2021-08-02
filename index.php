82643849992a4c04e314470c185b7313f6b37a70
yeeleong <yeeleong.hiew@gmail.com>
2021-08-01 22:39:59 -0700

    php

--- index.php
+++ index.php
@@ -1,1 +1,238 @@
-Hello PHP
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class="p-5">
+     
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+
+    <div class="row">
+        <div class="col-md-2">col-md-2
+        <?php
+        $name = "Hiew Yee Leong";
+
+        $newline = "1ついに全世界が完成しました。 2すべてを創造し終えると、神は七日目には休まれ、 3この日を祝福して、聖なる日と定めました。この日、天地創造の働きが完了したからです。
+        人間の創造
+        いう、あう、かう, 
+しぬ、よむ、よぶ,
+死ぬ、読む、呼ぶ, 
+のむ、飲む,
+たのむ、頼む, 
+とぶ、飛ぶ,
+ころぶ、転ぶ,
+はこぶ、運ぶ
+
+        ";
+        echo $name;
+        echo "<br/>";
+        echo $newline;
+        echo "<br/>";
+
+        $bible = "
+        1 わが子よ、あなたがもし／隣り人のために保証人となり、他人のために手をうって誓ったならば、
+        2 もしあなたのくちびるの言葉によって、わなにかかり、あなたの口の言葉によって捕えられたならば、
+        3 わが子よ、その時はこうして、おのれを救え、あなたは隣り人の手に陥ったのだから。急いで行って、隣り人にひたすら求めよ。
+        4 あなたの目を眠らせず、あなたのまぶたを、まどろませず、
+        5 かもしかが、かりゅうどの手からのがれるように、鳥が鳥を取る者の手からのがれるように、おのれを救え。
+        6 なまけ者よ、ありのところへ行き、そのすることを見て、知恵を得よ。
+        7 ありは、かしらなく、つかさなく、王もないが、
+        8 夏のうちに食物をそなえ、刈入れの時に、かてを集める。
+        9 なまけ者よ、いつまで寝ているのか、いつ目をさまして起きるのか。
+        10 しばらく眠り、しばらくまどろみ、手をこまぬいて、またしばらく休む。
+        11 それゆえ、貧しさは盗びとのようにあなたに来り、乏しさは、つわもののようにあなたに来る。
+        12 よこしまな人、悪しき人は／偽りの言葉をもって行きめぐり、
+        13 目でめくばせし、足で踏み鳴らし、指で示し、
+        14 よこしまな心をもって悪を計り、絶えず争いをおこす。
+        15 それゆえ、災は、にわかに彼に臨み、たちまちにして打ち敗られ、助かることはない。
+        16 主の憎まれるものが六つある、否、その心に、忌みきらわれるものが七つある。
+        17 すなわち、高ぶる目、偽りを言う舌、罪なき人の血を流す手、
+        18 悪しき計りごとをめぐらす心、すみやかに悪に走る足、
+        19 偽りをのべる証人、また兄弟のうちに争いをおこす人がこれである。
+        20 わが子よ、あなたの父の戒めを守り、あなたの母の教を捨てるな。
+        21 つねに、これをあなたの心に結び、あなたの首のまわりにつけよ。
+        22 これは、あなたが歩くとき、あなたを導き、あなたが寝るとき、あなたを守り、あなたが目ざめるとき、あなたと語る。
+        23 戒めはともしびである、教は光である、教訓の懲らしめは命の道である。
+        24 これは、あなたを守って、悪い女に近づかせず、みだらな女の、巧みな舌に惑わされぬようにする。
+        25 彼女の麗しさを心に慕ってはならない、そのまぶたに捕えられてはならない。
+        26 遊女は一塊のパンのために雇われる、しかし、みだらな女は人の尊い命を求める。
+        27 人は火を、そのふところにいだいて／その着物が焼かれないであろうか。
+        28 また人は、熱い火を踏んで、その足が、焼かれないであろうか。
+        29 その隣の妻と不義を行う者も、それと同じだ。すべて彼女に触れる者は罰を免れることはできない。
+        30 盗びとが飢えたとき、その飢えを満たすために盗むならば、人は彼を軽んじないであろうか。
+        31 もし捕えられたなら、その七倍を償い、その家の貨財を、ことごとく出さなければならない。
+        32 女と姦淫を行う者は思慮がない。これを行う者はおのれを滅ぼし、
+        33 傷と、はずかしめとを受けて、その恥をすすぐことができない。
+        34 ねたみは、その夫を激しく怒らせるゆえ、恨みを報いるとき、容赦することはない。
+        35 どのようなあがない物をも顧みず、多くの贈り物をしても、和らがない。";
+        echo $bible;
+    ?>
+        </div>
+        <div class="col-md-7">col-md-7</div>
+        <div class="col-md-3">col-md-3</div>
+
+
+    </div>
+
+    <div><p><?php include('jp.txt'); ?></p></div>
+    
+    <script>
+        var num = 10;
+        console.log(num);
+        //birth year of the person
+        var birthYear = 1964;
+        //current year for age calculation
+        var currentYear = 2021;
+        //create a variable called age and store the person's age in it
+
+        console.log(currentYear-birthYear)
+        //Create a function called calculate_total that takes two parameters:
+        function calculate_total(num_items, price){
+            return num_items*price+100;
+        }
+
+        console.log(calculate_total(5,5));
+
+        //"num_items" to represent the number of items, and "price" to represent the price per item.
+        //The function has to multiply these two parameters and add 100 to the total (for shipping costs).
+
+                /*In this Challenge we'll create a function that will whether a person is eligible to get a discount or not.
+Only people over 50 who live in postal code 1234 are eligible to get a discount.
+Create a function called check_eligibility that takes two parameters: age, and postalCode.
+If the person is eligible for a discount return true. If the person is not eligible, the function has to return false.
+(postal code is a number, not a string)*/
+
+        function check_eligibility(age,postalCode){
+            if (postalCode==1234){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        console.log(check_eligibility(13,1234));
+
+        // while loop
+        function print5numbers() {
+            var i=0;
+            while (i<= 5) {
+                j = i+=1;
+                console.log(i, '+', j);
+                i += 1;
+            }
+            console.log(i)
+        }
+        console.log(print5numbers());
+
+        //for loop
+        for (var i=1; i<=5; i++) {
+            if (i==3){
+                console.log('skip',i);
+                continue;
+            }
+            console.log('for loop', i);
+        }
+
+        // array = list in python
+        var list1 = [1,2,4,6,3,4];
+        var list2 = new Array('as', 12, list1);
+        console.log('list1:',list1);
+        console.log('list2:', list2);
+        list1.push(9,8);
+        console.log(list1);
+        list1.pop();
+        console.log(list1);
+
+        var list=[1,2,3,4,5,6,];
+
+        console.log(list);
+        list.splice(2,0,10,20);
+        console.log(list);
+        //from item 2, delete 0 item.
+        list.splice(2,2, 'a');
+        console.log(list);
+        //from item 2, delete 2 item2.
+
+        //iterating array
+        var list=[1,2,3,4,5,6,];
+        len=list.length;
+        console.log('list:',list);
+
+        var i=0;
+        while(i<len){
+            list[i]++;
+            i++;
+        }
+        console.log(list);
+        for(i=0; i<len; i++){
+            list[i]++;
+        }
+        console.log(list);
+        
+        // Dimension Array
+        var list=[[1,2,3],[4,4,5],[7,8,9]];
+        console.log(list);
+        console.log(list[1][2]);
+        console.log([2][0]);
+
+        list[2][2]=10;
+        console.log(list[2]);
+        
+        //using a while loop, traverse the array and multiply each price by 2
+        var prices = [10, 15, 25, 8, 4, 55, 99, 11, 15, 25, 5, 4, 65, 5, 10, 15, 7, 8, 4, 9, 100];
+        len=prices.length;
+        console.log(len);
+        var i=1;
+        while (i<len){
+            prices[i]*=2;
+            i++;
+        }
+        console.log(prices);
+
+        // object = array in python
+        var a={};
+        a.name='John';
+        a.num1=12;
+        a.num2=24
+        a.list=[1,2,3];
+        console.log(a);
+        console.log('His name is', a.name, 'who', a.num);
+        a.add = function(a,b){return a+b;};
+        console.log(a.num1,'+', a.num2, '=', a.add(a.num1,a.num2));
+        console.log(a.name, 'and', a['name']);
+
+        //JASON
+        var user={
+            name:'Peter',
+            age:23,
+            country:'USA',
+            profile:{
+                type:'private',
+                account:'premium'
+            }
+        };
+
+        console.log(user.profile.type);
+
+        // undefined
+        if ('notExist' in user) {
+            user.message='Exist';
+            }
+        else {
+            user.message='Not Exist';
+            }
+        console.log(user.message);
+        console.log(user.notExist);
+
+        console.log(!user.notExist);
+    </script>
+
+   
+</body>
+</html>
